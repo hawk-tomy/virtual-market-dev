6ed753b3494c4e50650bb7620d74d9fa79f6e612
tomy <67221751+hawk-tomy@users.noreply.github.com>
2021-10-08 08:25:50 +0900

    Create bank.py

---
+++ virtual-market-dev/lib/market/bank.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+
+class Bank:
+    def __init__(self, db):
+        pass
+
+    @classmethod
+    def from_column(cls, db, column):
+        pass
+
+    @classmethod
+    def from_new(cls, data):
+        pass
