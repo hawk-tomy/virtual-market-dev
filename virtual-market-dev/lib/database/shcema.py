3f20c96d82d0dfd7cfce9e87d22093e829ca249a
tomy <67221751+hawk-tomy@users.noreply.github.com>
2021-10-09 17:57:41 +0900

    Update shcema.py

--- virtual-market-dev/lib/database/shcema.py
+++ virtual-market-dev/lib/database/shcema.py
@@ -11,9 +11,13 @@ class Currency(Base):
 
     id = Column(Integer, primary_key=True)
     c_id = Column(Integer, unique=True)
-    onwer_id = Column(BigInteger)
-    bank_id = Column(Integer)
-    name = Column(String)
+    onwer_id = Column(BigInteger) #owner can not equal bank owner
+    bank_id = Column(Integer) #main bank
+
+    name = Column(String) #name of currency
     code = Column(String) #3char
     rate = Column(Float(asdecimal=True)) #rate for vbc
     created_at = Column(DateTime, server_default=func.now())
+    amount = Column(BigInteger) #amount of all currency. can inclease by owner.
+
+    ext = Column(postgresql.JSONB) #for addtional informations storage. this column must be use dict like.
